bb31f556f5879f9d2e1afdadf83f02ac2fcdfafe
Joaquin <quino.l85@gmail.com>
2025-03-14 09:31:28 +0100

    Modificacion de los ficheros actuales

--- src/main/java/org/example/fichero2.java
+++ src/main/java/org/example/fichero2.java
@@ -3,5 +3,6 @@ package org.example;
 public class fichero2 {
     public static void main(String[] args) {
         System.out.println("Este es el segundo fichero");
+        System.out.println("Fichero 3 modificado");
     }
 }
--- src/main/java/org/example/fichero3.java
+++ src/main/java/org/example/fichero3.java
@@ -3,5 +3,6 @@ package org.example;
 public class fichero3 {
     public static void main(String[] args) {
         System.out.println("Aqui tenemos el tercer fichero");
+        System.out.println("Fichero 3 modificado");
     }
 }
