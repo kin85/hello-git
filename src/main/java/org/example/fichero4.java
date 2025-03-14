2804f32b7aa3d886c3e473a8aea2000371b59516
Joaquin <quino.l85@gmail.com>
2025-03-14 10:16:25 +0100

    añadiendo login al fichero4

--- src/main/java/org/example/fichero4.java
+++ src/main/java/org/example/fichero4.java
@@ -4,5 +4,6 @@ public class fichero4 {
     public static void main(String[] args) {
         System.out.println("Este es el fichero numero 4");
         System.out.println("Version 2 del fichero numero 4");
+        System.out.println("Login");
     }
 }
