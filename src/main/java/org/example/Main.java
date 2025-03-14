d7c4c8cacfe282a1c010c917c700c280f35caeb1
Joaquin <quino.l85@gmail.com>
2025-03-14 11:50:32 +0100

    modificacion addreaddme

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!, Nuevo mundo");
+        System.out.println("Hello, World!, Nuevo mundo, despues de hacer el readme");
         System.out.println("Fichero uno modificado");
     }
 }
