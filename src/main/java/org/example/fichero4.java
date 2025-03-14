526626a04864b2ce8524b9734f2fdf1db799690a
Joaquin <quino.l85@gmail.com>
2025-03-14 10:19:05 +0100

    Ahora el fichero4 tiene la version numero 3

--- src/main/java/org/example/fichero4.java
+++ src/main/java/org/example/fichero4.java
@@ -3,6 +3,6 @@ package org.example;
 public class fichero4 {
     public static void main(String[] args) {
         System.out.println("Este es el fichero numero 4");
-        System.out.println("Version 2 del fichero numero 4");
+        System.out.println("Version 3 del fichero numero 4");
     }
 }
